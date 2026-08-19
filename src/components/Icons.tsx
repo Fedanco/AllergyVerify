f597dd7b49edaf3bdbffbd2d691123f0b1e37998
Federico Ancona <fancona27@gmail.com>
2026-08-19 12:42:24 +0200

    aggiunto link alla repo GitHub in Impostazioni

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -243,3 +243,13 @@ export function DocumentIcon({ className }: IconProps) {
     </svg>
   )
 }
+
+/** Logo GitHub: link al codice sorgente. Unica icona a silhouette piena tra
+ *  quelle a tratto, perché è un marchio riconoscibile e non va reinterpretata. */
+export function GithubIcon({ className }: IconProps) {
+  return (
+    <svg viewBox="0 0 24 24" className={className} fill="currentColor">
+      <path d="M12 .5C5.65.5.5 5.65.5 12c0 5.09 3.29 9.4 7.86 10.93.57.1.79-.25.79-.55 0-.27-.01-1.15-.02-2.09-3.2.7-3.88-1.36-3.88-1.36-.52-1.34-1.28-1.69-1.28-1.69-1.04-.72.08-.7.08-.7 1.15.08 1.76 1.19 1.76 1.19 1.03 1.76 2.7 1.25 3.36.96.1-.75.4-1.25.73-1.54-2.55-.29-5.24-1.28-5.24-5.7 0-1.26.45-2.29 1.19-3.09-.12-.29-.52-1.47.11-3.06 0 0 .97-.31 3.18 1.18a11.1 11.1 0 0 1 5.79 0c2.2-1.49 3.17-1.18 3.17-1.18.64 1.59.24 2.77.12 3.06.74.8 1.18 1.83 1.18 3.09 0 4.43-2.7 5.41-5.26 5.69.42.36.78 1.07.78 2.15 0 1.55-.01 2.8-.01 3.18 0 .3.21.66.8.55A11.5 11.5 0 0 0 23.5 12C23.5 5.65 18.35.5 12 .5Z" />
+    </svg>
+  )
+}
